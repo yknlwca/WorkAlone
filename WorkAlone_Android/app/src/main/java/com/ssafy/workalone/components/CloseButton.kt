d6b2e2ab154adc95d7ea35ba6c44b4662336a075
윤성현 <dbstjdgus35@naver.com>
2024-10-28 16:48:47 +0900

    feat: 운동 인증 완료 화면 개발(#6)

---
+++ WorkAlone_Android/app/src/main/java/com/ssafy/workalone/components/CloseButton.kt
@@ -0,0 +1,21 @@
+package com.ssafy.workalone.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.ssafy.workalone.R
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CloseButton(onClick: () -> Unit) {
+    Icon(
+        painter = painterResource(id = R.drawable.baseline_close_28),
+        contentDescription = "Close",
+        modifier = Modifier
+            .size(24.dp) // 아이콘 크기
+            .clickable { onClick() }
+    )
+}
---
+++ WorkAlone_Android/app/src/main/java/com/ssafy/workalone/components/CustomButton.kt
@@ -0,0 +1,38 @@
+package com.ssafy.workalone.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ssafy.workalone.ui.theme.WalkOneBlue500
+import com.ssafy.workalone.ui.theme.WalkOneGray50
+
+@Composable
+fun CustomButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            contentColor = WalkOneGray50,
+            containerColor = WalkOneBlue500
+        ),
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp) // 버튼 높이
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp
+        )
+    }
+
+}
