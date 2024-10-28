ef11427ce081b6ab728089bb682f7c2b22e9157c
이찬진 <cj8291@naver.com>
2024-10-28 09:55:07 +0900

    feat : spring boot 초기 세팅

---
+++ WorkAlone_BackEnd/src/main/java/com/ssawallafy/workalone_backend/WorkAloneBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ssawallafy.workalone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAloneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkAloneBackEndApplication.class, args);
+	}
+
+}
---
+++ WorkAlone_BackEnd/src/test/java/com/ssawallafy/workalone_backend/WorkAloneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssawallafy.workalone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkAloneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
