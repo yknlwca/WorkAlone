725f517e8eafabfbdc62a198469555afb2a39123
이찬진 <cj8291@naver.com>
2024-10-31 17:08:21 +0900

    feat : 초기 더미데이터 추가

--- WorkAlone_BackEnd/src/main/resources/data.sql
+++ WorkAlone_BackEnd/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO workalone.exercise_info (title, sub_title, basic_pose, movement, breath, exercise_type)
+INSERT INTO exercise_info (title, sub_title, basic_pose, movement, breath, exercise_type)
 VALUES ('스쿼트', '허벅지와 코어 근력 강화',
         '1. 발을 어깨너비로 벌리고, 발끝은 약간 바깥쪽을 향하게 합니다.\n2. 가슴은 펴고, 시선은 정면을 유지하며, 허리를 곧게 펴줍니다.',
         '1. 무릎을 구부리며 엉덩이를 뒤로 빼면서 천천히 앉습니다.\n2. 허벅지가 바닥과 평행할 때까지 내려가며, 무릎이 발끝을 넘지 않도록 주의합니다.\n3. 내려갔다가 다시 천천히 일어나면서 원래 자세로 돌아옵니다.',
@@ -16,10 +16,10 @@ VALUES ('스쿼트', '허벅지와 코어 근력 강화',
         '1. 복근에 힘을 주면서 상체를 들어 올려 무릎 쪽으로 향합니다.\n2. 천천히 내려가면서 원래 자세로 돌아옵니다.',
         '일어날 때 숨을 내쉬고, 내려갈 때 숨을 들이마십니다.', 'SITUP');
 
-INSERT INTO workalone.member (name, nickname, email, phone_number, gender, is_trainer, height, weight)
+INSERT INTO member (name, nickname, email, phone_number, gender, is_trainer, height, weight)
 VALUES ('John Doe', 'johnd', 'johndoe@example.com', '123-456-7890', true, false, 180, 75);
 
-INSERT INTO workalone.exercise (member_id, title, rest_time, exercise_set, set_detail, exercise_type, set_type, deleted)
+INSERT INTO exercise (member_id, title, rest_time, exercise_set, set_detail, exercise_type, set_type, deleted)
 VALUES (1, '스쿼트 루틴', 30, 2, 10, 'SQUAT', 'COUNT', 1),
        (1, '푸쉬업 챌린지', 60, 3, 15, 'PUSHUP', 'TIMER', 0),
        (1, '플랭크 버티기', 30, 4, 12, 'PLANK', 'COUNT', 0),
@@ -30,3 +30,15 @@ VALUES (1, '스쿼트 루틴', 30, 2, 10, 'SQUAT', 'COUNT', 1),
        (1, '싯업 루틴', 60, 4, 18, 'SITUP', 'TIMER', 0),
        (1, '플랭크 인내력', 30, 3, 9, 'PLANK', 'COUNT', 0),
        (1, '전신 통합 운동', 60, NULL, NULL, 'INTEGRATED', 'MIX', 0);
+
+-- 예시: 통합 운동 '통합 운동'은 '스쿼트 루틴', '푸쉬업 챌린지', '플랭크 버티기'로 구성
+INSERT INTO exercise_order (integrated_id, individual_id, order_position)
+VALUES ((SELECT id FROM exercise WHERE title = '통합 운동'), (SELECT id FROM exercise WHERE title = '스쿼트 루틴'), 3),
+       ((SELECT id FROM exercise WHERE title = '통합 운동'), (SELECT id FROM exercise WHERE title = '푸쉬업 챌린지'),1),
+       ((SELECT id FROM exercise WHERE title = '통합 운동'), (SELECT id FROM exercise WHERE title = '플랭크 버티기'), 2);
+
+-- 예시: 통합 운동 '전신 통합 운동'은 '싯업 시리즈', '스쿼트 챌린지', '푸쉬업 훈련'으로 구성
+INSERT INTO exercise_order (integrated_id, individual_id, order_position)
+VALUES ((SELECT id FROM exercise WHERE title = '전신 통합 운동'), (SELECT id FROM exercise WHERE title = '싯업 시리즈'), 2),
+       ((SELECT id FROM exercise WHERE title = '전신 통합 운동'), (SELECT id FROM exercise WHERE title = '스쿼트 챌린지'), 1),
+       ((SELECT id FROM exercise WHERE title = '전신 통합 운동'), (SELECT id FROM exercise WHERE title = '푸쉬업 훈련'), 3);
