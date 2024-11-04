50d4b821c97152f6cab0a356e1bcbaa733589591
윤성현 <dbstjdgus35@naver.com>
2024-11-04 15:54:47 +0900

    feat: 운동 완료 화면 데이터 연동 구현(#31)

--- WorkAlone_Android/app/src/main/java/com/ssafy/workalone/data/model/ExerciseRecord.kt
+++ WorkAlone_Android/app/src/main/java/com/ssafy/workalone/data/model/ExerciseRecord.kt
@@ -1,9 +1,16 @@
 package com.ssafy.workalone.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ExerciseRecord(
-    val id: Long = 0L,
+    @SerializedName("exerciseId")
+    val id: Long,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("exerciseCount")
     val exerciseCount: String,
+    @SerializedName("exerciseDuration")
     val exerciseDuration: Int,
+    @SerializedName("calorie")
     val calorie: Int
 )
