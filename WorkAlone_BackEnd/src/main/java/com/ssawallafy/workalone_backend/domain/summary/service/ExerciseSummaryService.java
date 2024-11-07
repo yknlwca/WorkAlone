eb799f761147953999220b2e3f97d2b4c4597fc3
이은우 <clairew.tech@gmail.com>
2024-11-07 10:15:12 +0900

    feat : 운동기록 조회 기능 추가 (#42)
    - 입력받은 날짜에 해당하는 운동 기록을 반환합니다.

--- WorkAlone_BackEnd/src/main/java/com/ssawallafy/workalone_backend/domain/summary/service/ExerciseSummaryService.java
+++ WorkAlone_BackEnd/src/main/java/com/ssawallafy/workalone_backend/domain/summary/service/ExerciseSummaryService.java
@@ -1,4 +1,10 @@
 package com.ssawallafy.workalone_backend.domain.summary.service;
 
+import java.time.LocalDate;
+
+import com.ssawallafy.workalone_backend.domain.summary.dto.ExerciseSummaryReadReq;
+import com.ssawallafy.workalone_backend.domain.summary.dto.ExerciseSummaryReadRes;
+
 public interface ExerciseSummaryService {
+	ExerciseSummaryReadRes readSummary(long memberId, LocalDate date);
 }
